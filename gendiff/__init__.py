a9e5e7ba425c08d3a5ce3b251e3c566799c44345
Valeria Lukovich <valeria_lukovic@mail.ru>
2023-10-17 15:13:01 +0200

    make library

---
+++ gendiff/__init__.py
@@ -0,0 +1,12 @@
+from gendiff.diff import generate_diff
+from gendiff.function_package.stylish import stylish
+from gendiff.function_package.plain import plain
+from gendiff.function_package.json import json_f
+
+
+__all__ = (
+    "generate_diff",
+    "stylish",
+    "plain",
+    "json_f",
+    )
