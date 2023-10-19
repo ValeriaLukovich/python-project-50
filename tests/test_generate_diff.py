f5d47e5dfaa2695bfccdd424bf050322e9332be2
Valeria Lukovich <valeria_lukovic@mail.ru>
2023-10-19 15:54:33 +0200

    fix errors

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,9 +1,4 @@
 from gendiff.diff import generate_diff
-from gendiff.function_package.plain import plain
-from gendiff.function_package.stylish import stylish
-from gendiff.function_package.json import json_f
-from tests.conftest import test_result, test_compare_file1, test_compare_file2, test_compare_nested_files, test_compare_nested_files3, test_compare_nested_files4, test_plain, test_plain1, test_json, test_json1
-import pytest
 
 
 def test_generate_diff(test_result, test_compare_file1, test_compare_file2, test_compare_nested_files, test_compare_nested_files3, test_compare_nested_files4, test_plain, test_plain1, test_json, test_json1):
