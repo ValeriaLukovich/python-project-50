79eaf114fa3c6d6bb25c87d84752387c71bd1607
Valeria Lukovich <valeria_lukovic@mail.ru>
2023-10-24 20:55:37 +0200

    make refactoring

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -4,18 +4,17 @@ from gendiff.diff import generate_diff
 
 @pytest.mark.parametrize(
     'file1,file2,form,expected',
-    [('tests/fixtures/files/file1.json', 'tests/fixtures/files/file2.json', "stylish", open('tests/fixtures/files/flat_stylish', "r").read()),
-    ('tests/fixtures/files/file1.yml', 'tests/fixtures/files/file2.yml', "stylish", open('tests/fixtures/files/flat_stylish', "r").read()),
-    ('tests/fixtures/files/file1.json', 'tests/fixtures/files/file1.yml', "stylish", open('tests/fixtures/files/flat_stylish_1', "r").read()),
-    ('tests/fixtures/files/file2.json', 'tests/fixtures/files/file2.yml', "stylish", open('tests/fixtures/files/flat_stylish_2', "r").read()),
-    ('tests/fixtures/files/file3.json', 'tests/fixtures/files/file4.yml', "stylish", open('tests/fixtures/files/nested_stylish', "r").read()),
-    ('tests/fixtures/files/file3.json', 'tests/fixtures/files/file3.yml', "stylish", open('tests/fixtures/files/nested_stylish_1', "r").read()),
-    ('tests/fixtures/files/file4.json', 'tests/fixtures/files/file4.yml', "stylish", open('tests/fixtures/files/nested_stylish_2', "r").read()),
-    ('tests/fixtures/files/file1.json', 'tests/fixtures/files/file1.json', "plain", ''),
-    ('tests/fixtures/files/file3.json', 'tests/fixtures/files/file4.yml', "plain", open('tests/fixtures/files/nested_plain', "r").read()),
-    ('tests/fixtures/files/file3.json', 'tests/fixtures/files/file4.yml', "json", open('tests/fixtures/files/nested_json', "r").read())],
+    [
+        ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', "stylish", open('tests/fixtures/flat_stylish', "r").read()),
+        ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', "stylish", open('tests/fixtures/flat_stylish', "r").read()),
+        ('tests/fixtures/file1.json', 'tests/fixtures/file1.yml', "stylish", open('tests/fixtures/flat_stylish_1', "r").read()),
+        ('tests/fixtures/file2.json', 'tests/fixtures/file2.yml', "stylish", open('tests/fixtures/flat_stylish_2', "r").read()),
+        ('tests/fixtures/file3.json', 'tests/fixtures/file4.yml', "stylish", open('tests/fixtures/nested_stylish', "r").read()),
+        ('tests/fixtures/file3.json', 'tests/fixtures/file3.yml', "stylish", open('tests/fixtures/nested_stylish_1', "r").read()),
+        ('tests/fixtures/file4.json', 'tests/fixtures/file4.yml', "stylish", open('tests/fixtures/nested_stylish_2', "r").read()),
+        ('tests/fixtures/file1.json', 'tests/fixtures/file1.json', "plain", ''),
+        ('tests/fixtures/file3.json', 'tests/fixtures/file4.yml', "plain", open('tests/fixtures/nested_plain', "r").read()),
+        ('tests/fixtures/file3.json', 'tests/fixtures/file4.yml', "json", open('tests/fixtures/nested_json', "r").read())],
 )
-
-
 def test_generate_diff(file1, file2, form, expected):
     assert generate_diff(file1, file2, form) == expected.rstrip()
